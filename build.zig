d6ec6b8e3d097f7c645aebfb217e4b5351bb2bf2
boris00035 <borisvanboxtel@gmail.com>
2024-06-27 01:22:27 +0200

    goede zig glfw + opengl example gevonden

--- build.zig
+++ build.zig
@@ -95,4 +95,17 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
     exe.root_module.addImport("mach-glfw", glfw_dep.module("mach-glfw"));
+
+    // Choose the OpenGL API, version, profile and extensions you want to generate bindings for.
+    const gl_bindings = @import("zigglgen").generateBindingsModule(b, .{
+        .api = .gl,
+        .version = .@"4.1",
+        .profile = .core,
+        .extensions = &.{ .ARB_clip_control, .NV_scissor_exclusive },
+    });
+
+    // Import the generated module.
+    exe.root_module.addImport("gl", gl_bindings);
+
+    b.installArtifact(exe);
 }
